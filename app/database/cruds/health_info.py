6bc1810e81279c5fd7161fdc3eb241e933b87ae4
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-10-10 09:02:58 +0900

    7일, 31일, 12개월만큼의 데이터를 100개 가져오도록 하는 함수 각각 추가

--- app/database/cruds/health_info.py
+++ app/database/cruds/health_info.py
@@ -13,13 +13,20 @@ class crud_health_info:
         db.refresh(db_protector)
         return db_protector
     
-    def get_health_info_by_user_id(db: Session, id, year, month, day):
+    def get_health_info_by_user_id_latest_one(db: Session, id):
+        db_health_info = (db.query(Health_info)
+                            .join(User, Health_info.user_id == id)
+                            .order_by(Health_info.measure_at.desc())
+                            .first())
+        return db_health_info
+    
+    def get_health_info_by_user_id_in_7days(db: Session, id, year, month, day):
         start_date = datetime(year=year, month=month, day=day)
-        end_date = datetime(year=year, month=month, day=day+1)
+        end_date = start_date + timedelta(days=7)
         db_health_info = (db.query(Health_info)
                             .join(User, Health_info.user_id == id)
                             .filter(Health_info.measure_at >= start_date, Health_info.measure_at < end_date)
-                            .all())
+                            .limit(100))
         res = []
         for health_info in db_health_info:
             res.append({
@@ -31,13 +38,13 @@ class crud_health_info:
             })
         return res
     
-    def get_health_info_by_user_id_in_month(db: Session, id, year, month):
-        start_date = datetime(year=year, month=month, day=1)
-        end_date = datetime(year=year, month=month+1, day=1)
+    def get_health_info_by_user_id_in_31days(db: Session, id, year, month, day):
+        start_date = datetime(year=year, month=month, day=day)
+        end_date = start_date + timedelta(days=31)
         db_health_info = (db.query(Health_info)
                             .join(User, Health_info.user_id == id)
                             .filter(Health_info.measure_at >= start_date, Health_info.measure_at < end_date)
-                            .all())
+                            .limit(100))
         res = []
         for health_info in db_health_info:
             res.append({
@@ -49,3 +56,20 @@ class crud_health_info:
             })
         return res
         
+    def get_health_info_by_user_id_in_12month(db: Session, id, year, month, day):
+        start_date = datetime(year=year, month=month, day=day)
+        end_date = datetime(year=year+1, month=month, day=day)
+        db_health_info = (db.query(Health_info)
+                            .join(User, Health_info.user_id == id)
+                            .filter(Health_info.measure_at >= start_date, Health_info.measure_at < end_date)
+                            .limit(100))
+        res = []
+        for health_info in db_health_info:
+            res.append({
+                "id": health_info.id,
+                "user_id": health_info.user_id,
+                "heart_rate": health_info.heart_rate,
+                "oxygen_saturation": health_info.oxygen_saturation,
+                "measure_at": health_info.measure_at
+            })
+        return res
