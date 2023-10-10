5f5b1135b5827ae4a6022f5b8e46b2e5dca51f2b
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-10-10 09:03:18 +0900

    pool_pre_ping 옵션 추가

--- app/database/session.py
+++ app/database/session.py
@@ -12,7 +12,7 @@ SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{}:{}@{}:{}/{}".format(
     settings.MARIADB_DATABASE,
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
